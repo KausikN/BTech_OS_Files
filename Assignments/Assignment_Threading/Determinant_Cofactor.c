4d175f46df12fd4a131a7896ffa031e86146670e
KausikN <coe17b010@iiitdm.ac.in>
2019-10-11 09:30:32 +0530

    Added External sorting algos

--- Assignments/Assignment_Threading/Determinant_Cofactor.c
+++ Assignments/Assignment_Threading/Determinant_Cofactor.c
@@ -2,93 +2,67 @@
 #include<stdlib.h>
 #include<pthread.h>
 
-int A[100][100];
-int I[100][100];
 int N;
 
-struct data
-{
-	int n;
-	int D;
-	int sign;
-	int f;
-	int mat[100][100];
-	int temp[100][100];
+int adj[100][100];
+float inv[100][100];
+int A[100][100];
+
+struct data{
+	int i;
+	int j;
 };
-// -----------------------------------------------------------
-void getCofactor(int *mat[], int *temp[], int p, int q, int n) 
-{ 
-    int i = 0, j = 0; 
 
-    for (int row = 0; row < n; row++) 
-    { 
-        for (int col = 0; col < n; col++) 
-        { 
-            if (row != p && col != q) 
-            { 
-                temp[i][j++] = mat[row][col]; 
+void cofactor(int mat[100][100], int temp[100][100], int p, int q, int n){
 
-                if (j == n - 1) 
-                { 
-                    j = 0; 
-                    i++; 
-                } 
-            } 
-        } 
-    } 
-} 
+	int i=0, j=0;
 
-void * detrunner(struct data * p)
-{
-	//printf("\nStarted %d-%d\n", i, j);
-
-	getCofactor(p->mat, p->temp, 0, p->f, p->n); 
-    p->D += p->sign * p->mat[0][p->f] * determinantOfMatrix(p->temp, p->n - 1);
-
-	pthread_exit(0);
-}
-
-int determinantOfMatrix(int *mat[], int n) 
-{
-    int D = 0;
-
-    if (n == 1) 
-        return mat[0][0]; 
-  
-    int temp[N][N];
-  
-    int sign = 1;
-
-    pthread_t tid[n];
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-
-	struct data d[n];
-
-	for(int i=0;i<n;i++)
-	{
-		d[i].n = n; d[i].D = D;
-		for(int j=0;j<n;j++)
-		{
-			for(int k=0;k<n;k++)\
-			{
-				d[i].mat[j][k] = mat[j][k];
+	for(int row= 0; row<n; row++){
+		for(int col=0; col<n; col++){
+			if(row!=p && col!=q){
+				temp[i][j++] = mat[row][col];
+				if(j==n-1){
+					j=0;
+					i++;
+				}
 			}
 		}
 	}
 
-    for (int f = 0; f < n; f++) 
-    { 
-        d[f].sign = sign; d[f].f = f;
-        pthread_create(&tid[f], &attr, detrunner, &d[f]);
+}
 
-        sign = -sign; 
-    }
+int determinant(int mat[100][100], int n){
+	
+	int D=0;
 
-    for(int i=0;i<n;i++) pthread_join(tid[i], NULL);
-  
-    return D; 
-} 
+	if(n==1)
+		return mat[0][0];
+
+	int temp[N][N];
+	int sign=1;
+
+
+	for(int f=0; f<n; f++){
+		cofactor(mat, temp, 0, f, n);
+		D += sign * mat[0][f] * determinant(temp, n-1);
+
+		sign=-sign;
+	}
+	return D;
+}
+
+void* runnercofacval(void *param){
+
+	int temp[N][N];
+	int sign=1;
+
+	struct data *data = param;
+	cofactor(A, temp, data->i, data->j, N);
+	sign = ((data->i + data->j)%2==0) ? 1: -1;
+	adj[data->j][data->i] = sign * (determinant(temp, N-1));
+
+	pthread_exit(0);
+}
 
 
 
@@ -111,42 +85,70 @@ int main(int argc, char *argv[])
 		{
 			for(int j=0;j<N;j++)
 			{
-				A[i][j] = atoi(argv[ind + i*j + j]);
+				A[i][j] = atoi(argv[ind + i*N + j]);
 			}
 		}
-		printf("Answer: \n");
 
-		//struct data p[M*N];
-
-		// pthread_t tid[M*N];
-		// pthread_attr_t attr;
-		// pthread_attr_init(&attr);
-
-		// int index = 0;
-
-		// for(int i=0;i<M;i++)
-		// {
-		// 	for(int j=0;j<N;j++)
-		// 	{
-		// 		p[index].i = i;
-		// 		p[index].j = j;
-		// 		pthread_create(&tid[index], &attr, runner, &p[index]);
-		// 		index++;
-		// 	}
-		// }
-
-		// for(int i=0;i<=M*N;i++) pthread_join(tid[i], NULL);
-
-		printf("DET: %d\n", determinantOfMatrix(A, N));
-
-		// Display
-		//printf("\nAnswer:\n");
+		printf("\nMatrix: \n");
 		for(int i=0;i<N;i++)
 		{
 			for(int j=0;j<N;j++)
+				printf("%d ", A[i][j]);
+			printf("\n");
+		}
+
+		pthread_t tid[N*N];
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+
+		if(N==1)
+		{
+			adj[0][0]=1;
+		}
+		else
+		{
+			int k=0;
+			for(int i=0; i<N; i++)
 			{
-				printf("%d ", I[i][j]);
+				for(int j=0; j<N; j++)
+				{
+					struct data *d = (struct data *) malloc(sizeof(struct data));
+
+					d->i = i;
+					d->j = j;
+
+					pthread_create(&tid[k],&attr, runnercofacval, d);
+					k++;
+				}
 			}
+			for(int i=0; i<k; i++)
+				pthread_join(tid[i],NULL);
+
+		}
+		printf("Adjoint:\n");
+		for(int i=0; i<N; i++)
+		{
+			for(int j=0; j<N; j++)
+				printf("%d ",adj[i][j]);
+			printf("\n");
+		}
+
+		int det = determinant(A, N);
+		if(det==0)
+		{
+			printf("Inverse doesn't exist\n");
+			return 0;
+		}
+
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++)
+				inv[i][j]= adj[i][j]/((float)det);
+
+		printf("Inverse:\n");
+		for(int i=0; i<N; i++)
+		{
+			for(int j=0; j<N; j++)
+				printf("%f ",inv[i][j]);
 			printf("\n");
 		}
 	}
